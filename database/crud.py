f1260167fe31b6bbf740570bd028e7d87742d43b
Gladlys <824765056@qq.com>
2022-11-22 01:37:20 +0800

    数据库连接

--- database/crud.py
+++ database/crud.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python3.9.5
 # -*- coding:utf-8 -*-
 
-'''
+"""
 @author：gladlys
 @date：2022/11/21 15:05
 @filename: crud.py
 @software: PyCharm
 @version: Python 3.9.5
-'''
+"""
+
+from sqlalchemy.orm import Session
+
+from database.db import session
+from database.model import User
+
+"""
+t_user
+"""
+
+
+def select_all_from_user(db: Session):
+    return db.query(User).all()
+
+
+if __name__ == "__main__":
+    print(select_all_from_user(session))
--- database/db.py
+++ database/db.py
@@ -1,30 +1,33 @@
 #!/usr/bin/env python3.9.5
 # -*- coding:utf-8 -*-
 
-'''
+"""
 @author：gladlys
 @date：2022/11/21 15:05
 @filename: db.py
 @software: PyCharm
 @version: Python 3.9.5
-'''
+"""
+
 import json
 
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+
 Base = declarative_base()
 
 config: {
     "": ""
 }
-with open('config.json', 'r') as fcc_file:
-    config = json.load(fcc_file)
+
+with open('config.json', 'r') as json_file:
+    config = json.load(json_file)
     print(json.dumps(config, indent=4))
-    
-pools="pools"
-pool="pool"
+
+pools = "pools"
+pool = "ecg"
 db_type = config[pools][pool]["db_type"]
 db_con = config[pools][pool]["db_con"]
 user = config[pools][pool]["user"]
@@ -37,8 +40,7 @@ engine = create_engine(db_url, echo=True)
 
 Session = sessionmaker(autocommit=True, autoflush=False, bind=engine)
 session = Session()
-
-
+session.close()
 # dependency,依赖
 def get_db():
     db_session = Session()
@@ -48,6 +50,7 @@ def get_db():
         print(e.__dict__)
     finally:
         db_session.close()
+
 
 if __name__ == "__main__":
     print(get_db())
